6978c6e78cdd740fbad3278ced659ed7d5a480b4
Sheddymae <125443020+Sheddymae@users.noreply.github.com>
2026-09-14 13:48:48 +0300

    chore(ui): remove unused glow button helper

--- components/GlowButton.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import type { ButtonHTMLAttributes } from 'react';
-
-export default function GlowButton({ className = '', ...props }: ButtonHTMLAttributes<HTMLButtonElement>) {
-  return <button {...props} className={`glow-button ${className}`} />;
-}
